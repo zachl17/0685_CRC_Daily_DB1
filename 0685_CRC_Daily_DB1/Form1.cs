af4557c3a5429e3df6e30c226e8a0aeb546d80e4
Zachary Luedtke <ZacharyL@americollect.com>
2021-03-04 10:08:56 -0600

    add sql connection fields

--- 0685_CRC_Daily_DB1/Form1.cs
+++ 0685_CRC_Daily_DB1/Form1.cs
@@ -12,9 +12,28 @@ namespace _0685_CRC_Daily_DB1
 {
     public partial class Form1 : Form
     {
+        //sql connection
+        static string testDataSource = "AMCDB04-T";
+        static string productionDataSource = "AMCDB03";
+        static string testInitialCatalog = "DevelopmentTesting";
+        static string productionInitialCatalog = "";
+        static string integratedSecurity = "True";
+        static string multipleActiveResultSets = "True";
+
+        //sql tables
+        string dataTable = "[dbo].[AllWorkers.CRCDaily_Data]";
+        string doneDataTable = "[dbo].[AllWorkers.CRCDaily_Done]";
+        string sqlConnStr = $"Data Source={testDataSource};Initial Catalog={testInitialCatalog};Integrated Security={integratedSecurity};MultipleActiveResultSets={multipleActiveResultSets};";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //Fills the form with data from the worker's table
+        void PopulateForm()
+        {
+
         }
     }
 }
